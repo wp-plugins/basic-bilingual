de9063b674efaea67ff2f868619b0f70e5d53505
Steph <Steph@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-11-14 09:13:31 +0000

    updated basic bilingual readme and 0.4 tag
    git-svn-id: https://plugins.svn.wordpress.org/basic-bilingual/tags/0.4@173406 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- basic-bilingual.php
+++ basic-bilingual.php
@@ -1,8 +1,9 @@
 <?php
 /*
 Plugin Name: Basic Bilingual
-Plugin URI: http://climbtothestars.org/archives/2007/11/30/basic-bilingual-03-for-multilingual-blogging/Description: Makes managing your blog with two languages less cumbersome.
-Version: 0.33
+Plugin URI: http://climbtothestars.org/wordpress/basic-bilingual/
+Description: Makes managing your blog with two languages less cumbersome.
+Version: 0.4
 Author: Stephanie Booth
 Author URI: http://climbtothestars.org/
 
@@ -26,7 +27,7 @@ Author URI: http://climbtothestars.org/
 INFORMATION:
 ============
 
-View http://climbtothestars.org/archives/2007/11/30/basic-bilingual-03-for-multilingual-blogging/ for information about this plugin, what it does and how to use it. In short, it has to do with blogging in more than one language.
+View http://climbtothestars.org/wordpress/basic-bilingual/ for information about this plugin, what it does and how to use it. In short, it has to do with blogging in more than one language.
 
 CHANGELOG:
 ==========
@@ -39,14 +40,15 @@ CHANGELOG:
 0.3  - Added stripslashes to get rid of slash problem
      - No need to add template tag anymore for other-excerpt -- added automagically (30.11.2007)
      - Added class to excerpt first-child
-0.31 - Attempted to fix vanishing excerpts problem -- see http://markjaquith.wordpress.com/2007/01/28/ 
+0.31 - Attempted to fix vanishing excerpts problem -- see http://markjaquith.wordpress.com/2007/01/28/
        authorization-and-intentionorigination-verification-when-using-the-edit_post-hook/
-0.32 - Replaced the ugly "language box" in the admin section with a pretty DBX box. Drag it to the top
-	   of the page!  
-0.33 - Fixed a bunch of stuff, code provided kindly by Tim Isenheim http://www.freshlabs.de/journal/	
-	 - Half-arsed attempt to make the interface look prettier (if you know how to make 
-	   pretty posboxes let me know) -- 30.01.2009   
-	      
+0.32 - Replaced the ugly "language box" in the admin section with a pretty DBX box. Drag it to the top of the page!
+0.33 - Fixed a bunch of stuff, code provided kindly by Tim Isenheim http://www.freshlabs.de/journal/
+	 - Half-arsed attempt to make the interface look prettier (30.01.2009)
+0.4  - Modified the be fully compatible with the last WordPress versions: drag the language and other-excerpt boxes
+       -- code provided by Luca Palli http://video.monte-ceneri.org/ (27.09.2009)
+	 - Add the "Allow empty language" option on the new Options page (27.09.2009, Luca Palli)
+
 
 SETTINGS:
 =========
@@ -55,14 +57,18 @@ Replace "en" and "fr" with your two languages in the array below, with two-lette
 
 */
 
-$bb_languages=array('en', 'fr');
+$bb_languages = array('en', 'fr');
+
+$bb_language_field = 'language';
+$bb_other_excerpt_field = 'other-excerpt';
+$bb_allow_empty_language_option = 'bb_allow_empty_language';
 
 /*
 
 CSS:
 ====
 
-You might want define CSS rules similar to these for your stylesheet:
+You might want to define CSS rules similar to these for your stylesheet:
 
 .other-excerpt {
 	font-style: italic;
@@ -102,29 +108,27 @@ div.hentry:lang(en) .entry-title:after {
 
 */
 
-
-
 // retrieve the language of the post
 function bb_get_the_language()
-{	
-	$post_language=get_post_custom_values("language");
-	$language=$post_language['0'];
-	return($language);
-} 
+{
+	global $bb_language_field;
+	$post_language = get_post_custom_values($bb_language_field);
+	$language = $post_language['0'];
+	return $language;
+}
 
 // return the other language (the one the post isn't in)
 function bb_get_the_other_language()
 {
 	global $bb_languages;
-	if(bb_get_the_language()==$bb_languages[0])
+	if (bb_get_the_language() == $bb_languages[0])
 	{
-		$other_language=$bb_languages[1];
-	}else{
-		$other_language=$bb_languages[0];
+		$other_language = $bb_languages[1];
+	} else {
+		$other_language = $bb_languages[0];
 	}
-	return($other_language);
+	return $other_language;
 }
-
 
 // TEMPLATE FUNCTIONS
 
@@ -134,7 +138,7 @@ function bb_the_time($format="%A %d.%m.%Y<br />%Hh%M")
 {
 	global $post;
 	
-	$language=bb_get_the_language();
+	$language = bb_get_the_language();
 	
 	// setlocale needs the language in this format
 	$code = $language . '_' . strtoupper($language);
@@ -143,42 +147,44 @@ function bb_the_time($format="%A %d.%m.%Y<br />%Hh%M")
 	setlocale(LC_TIME, $code);
 	
 	// write it out -- this was lifted from the_time() iirc
-	$wp_time=$post->post_date;
-	$timestamp=strtotime($wp_time);
-	$result=strftime($format, $timestamp);
+	$wp_time = $post->post_date;
+	$timestamp = strtotime($wp_time);
+	$result = strftime($format, $timestamp);
 	print($result);
 }
 
 // this one outputs the language
 function bb_the_language()
 {
-	$language=bb_get_the_language();
+	$language = bb_get_the_language();
 	print($language);
 }
 
 // this outputs the other language excerpt
 function bb_get_the_other_excerpt($before='<div class="other-excerpt" lang="%lg"><p class="oe-first-child">', $after='</p></div>')
 {
-	$post_other_excerpt=get_post_custom_values("other-excerpt");
-	$the_other_excerpt=$post_other_excerpt['0'];
+	global $bb_other_excerpt_field;
+	
+	$post_other_excerpt = get_post_custom_values($bb_other_excerpt_field);
+	$the_other_excerpt = $post_other_excerpt['0'];
 	
 	// make sure there is an excerpt to display
 	if(!empty($the_other_excerpt))
 	{
 		// this is the excerpt language (easy, because it's bilingual)
-		$excerpt_language=bb_get_the_other_language();
+		$excerpt_language = bb_get_the_other_language();
 		
 		// add a nice little lang attribute where asked for
-		$before=str_replace('%lg', $excerpt_language, $before);
-		$after=str_replace('%lg', $excerpt_language, $after); // doubt this is needed!
+		$before = str_replace('%lg', $excerpt_language, $before);
+		$after = str_replace('%lg', $excerpt_language, $after); // doubt this is needed!
 		
 		// add separators so that newsreaders which don't get formatting know when the post starts
-		$post_language=bb_get_the_language();
-		$post_separator_after="<p class=\"bb-post-separator\"><strong>[$post_language]</strong></p>";
-		$post_separator_before="<p class=\"bb-post-separator\"><strong>[$excerpt_language]</strong></p>";
+		$post_language = bb_get_the_language();
+		$post_separator_after = "<p class=\"bb-post-separator\"><strong>[$post_language]</strong></p>";
+		$post_separator_before = "<p class=\"bb-post-separator\"><strong>[$excerpt_language]</strong></p>";
 		// stick everything together
 		$the_other_excerpt = $post_separator_before . $before . $the_other_excerpt . $after . $post_separator_after;
-		return($the_other_excerpt);
+		return $the_other_excerpt;
 	}
 }
 
@@ -189,12 +195,10 @@ function bb_the_other_excerpt()
 }
 
 // automatic insertion of other-excerpt
-
 function bb_embed_other_excerpt($content) {
-        $content = bb_get_the_other_excerpt() . $content;
-        return $content;
+	$content = bb_get_the_other_excerpt() . $content;
+	return $content;
 }
-
 
 // automatic insertion of lang attribute on post div
 /* function bb_embed_lang($buffer) {
@@ -205,171 +209,223 @@ function bb_insert_lang_attribute($buffer) {
  	$search='/(class="(.)*hentry(.)*")/';
  	$replace=\\1 . ' lang="' . bb_get_the_language() . '"';
  	return preg_replace($search, $replace, $buffer);	
-} not sure this will work */
-
+} not sure this will work, and depends on the template */
 
 // ADMIN TWEAKING
 
-// output textarea to easily add other-excerpt in admin menu (addition to the post form)
-// deprecated
-function add_other_excerpt_textarea() {
-	
-	global $post;
-	
-	$excerpt = get_post_meta($post->ID, 'other-excerpt', true);
-	
-	echo '<fieldset id="postotherexcerpt" style="clear: both;"><legend>' . __('Other Language Excerpt', 'BasicBilingual') . '</legend>';
-	echo '<div><textarea rows="4" cols="80" name="other-excerpt" id="other-excerpt">';
-	print($excerpt);
-	echo '</textarea></div></fieldset>';
-	// hidden field to avoid vanishing meta
- echo '<input type="hidden" name="bunny-key" id="bunny-key" value="' . wp_create_nonce('bunny') . '" />'; 
-}
-
-// new function, to make the textarea prettier and more 2.7-compatible
-
-function add_elegant_other_excerpt_textarea() {
-	
-	global $post;
-	
-	$excerpt = get_post_meta($post->ID, 'other-excerpt', true);
-echo '
-<div id="postother-excerpt" class="postbox">
-<h3>Other Language Excerpt</h3>
-<div class="inside">
-
-<label class="hidden" for="other-excerpt">Other Language Excerpt</label><textarea rows="10" cols="80" name="other-excerpt" id="other-excerpt">';
-print($excerpt);
-echo '</textarea>
-<p>Write an excerpt of your post in the other language you use on your blog. Short and sweet, or long and detailed.</p>
-</div>
-</div>';
-
-// hidden field to avoid vanishing meta
- echo '<input type="hidden" name="bunny-key" id="bunny-key" value="' . wp_create_nonce('bunny') . '" />'; 
-}
-
-
-
-
-
-
-
-
-// DEPRECATED this one outputs a little box for typing in the post language (admin pages)
-function add_language_box()
+/**
+ * Adds a custom section to the Post and Page edit screens
+ */
+function bb_add_custom_boxes()
 {
- 	global $bb_languages;
- 	global $post;
- 	
- 	// retrieving existing language, or setting to default if new post
- 	$current_language=get_post_meta($post->ID, 'language', true);
- 	if(empty($current_language))
- 	{
- 		$current_language=$bb_languages[0];
- 	}
-
- 	print('<fieldset id="languagediv" style="height: 3.5em; width: 5em; position: absolute; top: 10em; left: 42em;">
-      <legend>');
-      echo __('Language');
-      print('</legend>
-	  <div><input type="text" name="language" size="7" value="');
-	  print($current_language);
-	  print('" id="language" /></div>
-</fieldset>');
+	if ( function_exists('add_meta_box'))
+	{
+		add_meta_box('bb_language', __( 'Language', 'bb_textdomain' ), 'bb_language_box', 'post', 'side');
+		add_meta_box('bb_language', __( 'Language', 'bb_textdomain' ), 'bb_language_box', 'page', 'side');
+		
+		add_meta_box('bb_other_excerpt', __( 'Other Language Excerpt', 'bb_textdomain' ), 'bb_other_excerpt_box', 'post', 'normal');
+		add_meta_box('bb_other_excerpt', __( 'Other Language Excerpt', 'bb_textdomain' ), 'bb_other_excerpt_box', 'page', 'normal');
+	} else {
+		add_action('dbx_page_sidebar', 'bb_language_old_box');
+		add_action('dbx_post_sidebar', 'bb_language_old_box');
+		
+		add_action('simple_edit_form', 'bb_other_excerpt_old_box');
+		add_action('edit_form_advanced', 'bb_other_excerpt_old_box');
+		add_action('edit_page_form', 'bb_other_excerpt_old_box');
+	}
 }
 
-// this outputs a little box for typing in the post language, in the admin pages sidebar (posts and pages)
-function bb_add_dbx_language_box()
+/**
+ * Prints the inner fields for the language post/page section
+ */
+function bb_language_box()
 {
- 	global $bb_languages;
- 	global $post;
-
+	global $post;
+	global $bb_languages;
+	global $bb_language_field;
+	global $bb_allow_empty_language_option;
+	
 	// retrieving existing language, or setting to default if new post
- 	$current_language=get_post_meta($post->ID, 'language', true);
- 	if(empty($current_language))
- 	{
- 		$current_language=$bb_languages[0];
- 	}
- 	
- 	print('<div id="post-lang" class="postbox">
- 	<h3>');
-      echo __('Language');
-      print('</h3>
-	  <div class="inside"><input type="text" name="language" size="7" value="');
-	  print($current_language);
-	  print('" id="language" /> <label for="language">2-letter code</label></div>
-</div>');
+	$current_language = get_post_meta($post->ID, $bb_language_field, true);
+	if (empty($current_language) && !get_option($bb_allow_empty_language_option))
+	{
+		$current_language=$bb_languages[0]; // LP: to support empty language
+	}
+	
+	// Use nonce for verification
+	echo '<input type="hidden" name="bb_noncename" id="bb_noncename" value="' . wp_create_nonce(plugin_basename(__FILE__)) . '" />';
+	
+	// The actual fields for data entry
+	echo '<input type="text" name="' . $bb_language_field . '" value="' . $current_language . '" size="7" id="' . $bb_language_field . '" /> ';
+	echo '<label for="bb_language">' . __("2-letter code", 'bb_textdomain' ) . '</label>';
 }
 
+/**
+ * Prints the edit form for pre-WordPress 2.5 post/page
+ */
+function bb_language_old_box()
+{
+	echo '<div id="post-lang" class="postbox"><h3>' . __('Language') . '</h3>';
+	echo '<div class="inside">' . bb_language_box() . '</div></div>';
+}
 
+/**
+ * Prints the inner fields for the other excerpt_box post/page section
+ */
+function bb_other_excerpt_box()
+{
+	global $post;
+	global $bb_other_excerpt_field;
+	
+	$excerpt = get_post_meta($post->ID, $bb_other_excerpt_field, true);
+	
+	// Use nonce for verification
+	echo '<input type="hidden" name="bb_noncename" id="bb_noncename" value="' . wp_create_nonce(plugin_basename(__FILE__)) . '" />';
+	
+	// The actual fields for data entry
+	echo '<textarea rows="10" cols="80" name="' . $bb_other_excerpt_field . '" id="' . $bb_other_excerpt_field . '" style="width: 100%;">' . $excerpt . '</textarea>';
+	echo '<p>Write an excerpt of your post in the other language you use on your blog. Short and sweet, or long and detailed.</p>';
+}
+
+/**
+ * Prints the edit form for pre-WordPress 2.5 post/page
+ */
+function bb_other_excerpt_old_box()
+{
+	echo '<div id="post-lang" class="postbox"><h3>' . __('Other Language Excerpt') . '</h3>';
+	echo '<div class="inside">' . bb_other_excerpt_box() . '</div></div>';
+}
+
+/** 
+ * Adds a custom submenu to the Options
+ */
+function bb_add_options()
+{
+	// Add a new submenu under Options
+	add_options_page('Basic Bilingual', 'Basic Bilingual', 'administrator', 'bb_options', 'bb_options_page');
+}
+
+/**
+ * Displays the page content for the Basic Bilingual Options submenu
+ */
+function bb_options_page()
+{
+	global $bb_allow_empty_language_option;
+	
+	// variables for the field and option names
+	$opt_name = $bb_allow_empty_language_option;
+	$hidden_field_name = 'bb_submit_hidden';
+	
+	// Read in existing option value from database
+	$opt_val = get_option($opt_name);
+	
+	// See if the user has posted us some information
+	// If they did, this hidden field will be set to 'Y'
+	if ($_POST[$hidden_field_name] == 'Y')
+	{
+		// Read their posted value
+		$opt_val = $_POST[$opt_name];
+		
+		// Save the posted value in the database
+		update_option($opt_name, $opt_val);
+		
+		// Put an options updated message on the screen
+		echo '<div class="updated"><p><strong>' . __('Options saved.', 'bb_textdomain') . '</strong></p></div>';
+	}
+	
+	// Now display the options editing screen
+	echo '<div class="wrap">';
+	// header
+	echo "<h2>" . __( 'Basic Bilingual', 'bb_textdomain' ) . "</h2>";
+	// options form
+	echo '<form name="form1" method="post" action="">';
+	echo '<input type="hidden" name="' . $hidden_field_name . '" value="Y">';
+	echo '<p>' . __("Allow empty language:", 'bb_textdomain');
+	echo '<input type="checkbox" name="' . $opt_name . '" value="true"';
+	if ($opt_val) 
+	{
+		echo ' checked="checked"';
+	}//'"' . $opt_val .'"
+	echo ' />';
+	echo '</p><hr />';
+	echo '<p class="submit"><input type="submit" name="Submit" value="' . __('Update Options', 'bb_textdomain') . '" /></p>';
+	echo '</form>';
+	echo '</div>';
+}
 
 // ACTION FUNCTIONS
+
+/**
+ * When the post is saved, saves our custom data
+ */
+function bb_save_postdata($id)
+{	
+	global $bb_language_field;
+	global $bb_other_excerpt_field;
+	
+	// verify this came from the our screen and with proper authorization,
+	// because save_post can be triggered at other times
+	if (!wp_verify_nonce($_POST['bb_noncename'], plugin_basename(__FILE__)))
+	{
+		return $id;
+	}
+	
+	// verify if this is an auto save routine. If it is our form has not been
+	// submitted, so we dont want to do anything
+	/*if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+	{
+		return $post_id;
+	}*/
+	
+	// Check permissions
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can( 'edit_page', $id ))
+		{
+			return $id;
+		}
+	} else {
+		if (!current_user_can( 'edit_post', $id ))
+		{
+			return $id;
+		}
+	}
+	
+	// update language and other language excerpt custom fields
+	bb_update_meta($id, $bb_language_field);
+	bb_update_meta($id, $bb_other_excerpt_field);
+}
 
 // general custom field update function
 function bb_update_meta($id, $field)
 {
-	// authorization to avoid vanishing meta    if ( !current_user_can('edit_post', $id) )        return $id;    // origination and intention to avoid vanishing meta    if ( !wp_verify_nonce($_POST['bunny-key'], 'bunny') )        return $id;
 	$setting = stripslashes($_POST[$field]);
-	$meta_exists=update_post_meta($id, $field, $setting);
-	if(!$meta_exists)
+	$meta_exists = update_post_meta($id, $field, $setting);
+	if (!$meta_exists)
 	{
-		add_post_meta($id, $field, $setting);	
+		add_post_meta($id, $field, $setting);
 	}
 }
-
-
-// update language custom field
-function bb_update_language($id, $post)
-{
-	if ($post->post_type == 'revision') {
-		return;
-	}
-	bb_update_meta($id, "language");
-}
-
-// update other language excerpt custom field
-function bb_update_other_excerpt($id, $post)
-{
-	if ($post->post_type == 'revision') {
-		return;
-	}
-	bb_update_meta($id, "other-excerpt");
-}
-
-//add_action('simple_edit_form', 'add_other_excerpt_textarea');
-//add_action('edit_form_advanced', 'add_other_excerpt_textarea');
-
-add_action('simple_edit_form', 'add_elegant_other_excerpt_textarea');
-add_action('edit_form_advanced', 'add_elegant_other_excerpt_textarea');
-
-
-
-// add_action('simple_edit_form', 'add_language_box');
-// add_action('edit_form_advanced', 'add_language_box');
-// add_action('edit_page_form', 'add_language_box');
-add_action('edit_page_form', 'add_other_excerpt_textarea');
-
-add_action('dbx_page_sidebar', 'bb_add_dbx_language_box');
-add_action('dbx_post_sidebar', 'bb_add_dbx_language_box');
-
-
-add_action('edit_post', 'bb_update_language', 1, 2);
-add_action('save_post', 'bb_update_language', 1, 2);
-add_action('publish_post', 'bb_update_language', 1, 2);
-
-add_action('edit_post', 'bb_update_other_excerpt', 1, 2);
-add_action('save_post', 'bb_update_other_excerpt', 1, 2);
-add_action('publish_post', 'bb_update_other_excerpt', 1, 2);
 
 add_action('the_content', 'bb_embed_other_excerpt');
+
+/* Use the admin_menu action to define the custom boxes */
+add_action('admin_menu', 'bb_add_custom_boxes');
+
+/* Use the save_post action to do something with the data entered */
+add_action('edit_post', 'bb_save_postdata', 1, 2);
+add_action('save_post', 'bb_save_postdata', 1, 2);
+add_action('publish_post', 'bb_save_postdata', 1, 2);
+
+// Use the admin_menu action to define the admin menus
+add_action('admin_menu', 'bb_add_options');
 
 /* not sure this will work
 function bb_got_hentry()
 {
 	return false;
-	}
+}
 if(bb_got_hentry())
-{	add_action('template_redirect', 'bb_embed_lang');
+{
+	add_action('template_redirect', 'bb_embed_lang');
 } */
 ?>
