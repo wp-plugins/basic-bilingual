d99c9ff30905e916b9631fab572b24f53822e7bf
cvedovini <cvedovini@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-16 05:30:39 +0000

    adding some blurb for the translators on the admin page
    git-svn-id: https://plugins.svn.wordpress.org/basic-bilingual/trunk@805231 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- basic-bilingual.php
+++ basic-bilingual.php
@@ -5,7 +5,7 @@ Plugin URI: http://climbtothestars.org/wordpress/basic-bilingual/
 Description: Makes managing your blog with two languages less cumbersome.
 Author: Stephanie Booth
 Author URI: http://climbtothestars.org/
-Version: 1.1
+Version: 1.1.1
 
 # The code in this plugin is free software; you can redistribute the code aspects of
 # the plugin and/or modify the code under the terms of the GNU Lesser General
--- class-admin.php
+++ class-admin.php
@@ -291,6 +291,9 @@ class BasicBilingualAdmin {
 							<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 							</form>
 						</div>
+						<p><?php _e('We also need volunteers to translate that plugin into more languages.
+								If you wish to help then contact <a href="https://twitter.com/stephtara">@stephtara</a>
+								on Twitter or see <a href="http://climbtothestars.org/contact/">Stephanie\'s contact page</a>.', 'wp-linkedin'); ?></p>
 					</div> <!-- .inside -->
 				</div> <!-- .postbox -->
 				<div>
